b22a7a4651c73b21407d1c9057f9ec6c0e3f43df
KrisHub94 <kristianhuber@hotmail.com>
2023-05-09 11:19:50 +0200

    a

--- src/main/java/com/codecool/marsexploration/logic/MapManager.java
+++ src/main/java/com/codecool/marsexploration/logic/MapManager.java
@@ -6,13 +6,14 @@ import com.codecool.marsexploration.data.MapConfig;
 import java.util.HashMap;
 
 public class MapManager {
+    private final String EMPTY_TILE = " ";
     public HashMap<Coordinate, String> createMap(MapConfig mapConfig) {
         int width = mapConfig.mapWidth();
         HashMap<Coordinate, String> map = new HashMap<>();
         for(int x = 1; x <= width; x++) {
             for(int y = 1; y <= width; y++) {
                 Coordinate coordinate = new Coordinate(x, y);
-                map.put(coordinate, " ");
+                map.put(coordinate, EMPTY_TILE);
             }
         }
         return map;

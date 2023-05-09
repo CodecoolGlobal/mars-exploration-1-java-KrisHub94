beec71ac8138bd94f7a8fee4550ed2b6e435a4c2
KrisHub94 <kristianhuber@hotmail.com>
2023-05-09 10:30:22 +0200

    added MapManager

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -1,10 +1,20 @@
 package com.codecool.marsexploration;
 
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.MapConfig;
+import com.codecool.marsexploration.logic.MapManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
         Random random = new Random();
         System.out.println(random.nextInt(42));
+        MapConfig mapConfig = new MapConfig("test", 30, new ArrayList<>(), new ArrayList<>());
+        MapManager mapManager = new MapManager();
+        HashMap<Coordinate, String> map = mapManager.createMap(mapConfig);
+        System.out.println(map);
     }
 }
---
+++ src/main/java/com/codecool/marsexploration/logic/MapManager.java
@@ -0,0 +1,20 @@
+package com.codecool.marsexploration.logic;
+
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.data.MapConfig;
+
+import java.util.HashMap;
+
+public class MapManager {
+    public HashMap<Coordinate, String> createMap(MapConfig mapConfig) {
+        int width = mapConfig.mapWidth();
+        HashMap<Coordinate, String> map = new HashMap<>();
+        for(int x = 1; x <= width; x++) {
+            for(int y = 1; y <= width; y++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                map.put(coordinate, " ");
+            }
+        }
+        return map;
+    }
+}

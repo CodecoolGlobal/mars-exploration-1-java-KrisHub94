9a1f597efd949b4b7b97b8ce153cead6ae93fa06
Martin Egert <m.egert82@gmail.com>
2023-05-11 00:53:19 +0200

    created file for shapeGeneratorTest

---
+++ src/test/java/com/codecool/marsexploration/logic/ShapeGeneratorTest.java
@@ -0,0 +1,16 @@
+package com.codecool.marsexploration.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapeGeneratorTest {
+
+    @Test
+    void createShape2() {
+    }
+
+    @Test
+    void createEmptyShape() {
+    }
+}
